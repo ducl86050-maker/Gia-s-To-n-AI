c207a559da1d0f44aaa7a68f5b0d856f4b798794
ducl86050-maker <ducl86050@gmail.com>
2026-06-23 15:43:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿import streamlit as st
+﻿import streamlit as st
 import google.generativeai as genai
 
 st.title("🤖 Chào mừng em đến với gia sư Toán Soleil!! (⁀ᗢ⁀) ")
@@ -6,7 +6,6 @@ st.title("🤖 Chào mừng em đến với gia sư Toán Soleil!! (⁀ᗢ⁀) "
 # Kiểm tra Secrets
 if "GOOGLE_API_KEY" in st.secrets:
     genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-    # Dòng này phải nằm sát lề trái, thẳng hàng với dòng if
     model = genai.GenerativeModel('gemini-2.5-flash')
 else:
     st.error("Chưa cấu hình API Key trong Secrets!")
@@ -19,7 +18,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("hãy để chị giải quyết thắc mắc của em nhé! (´｡• ω •｡`) ♡"):
+if prompt := st.chat_input("Hãy để chị giải quyết thắc mắc của em nhé! (´｡• ω •｡`) ♡"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
